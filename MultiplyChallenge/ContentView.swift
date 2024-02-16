025e1d2430b3c874f4169bbb032718976b4b6679
sbibla <saar.bibla@gmail.com>
2024-02-15 16:55:36 -0800

    Initial Commit

---
+++ MultiplyChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MultiplyChallenge
+//
+//  Created by Saar Bibla on 2/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
